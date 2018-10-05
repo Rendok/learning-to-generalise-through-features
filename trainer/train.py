6302c44fa376f3f2a0be1a7b7f501a68b4f53439
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2018-10-05 16:45:14 +1000

    new trainer

--- trainer/train.py
+++ trainer/train.py
@@ -1,12 +1,10 @@
 import ray
-from ray.tune.logger import pretty_print
 from ray.tune.registry import register_env
-from ray.rllib.agents import ddpg
+import ray.tune as tune
 import argparse
 
-
+# needs to register a custom environment
 def env_creator(renders=False):
-    import gym
     import gym_kuka_multi_blocks.envs.kuka_multi_blocks_gym_env as e
     env = e.KukaMultiBlocksEnv(renders=renders,
                                numObjects=3,
@@ -31,7 +29,7 @@ if __name__ == '__main__':
         '--num_workers',
         help='The number of workers',
         type=int,
-        default=4
+        default=2
     )
 
     parser.add_argument(
@@ -41,33 +39,35 @@ if __name__ == '__main__':
         default=True
     )
 
+    parser.add_argument(
+        '--run',
+        help='Algorithm to run',
+        type=str,
+        default="PPO"
+    )
+
     args = parser.parse_args()
 
     # register a custom environment
-    register_env("my_env", env_creator)
+    register_env("KukaMultiBlocks-v0", env_creator)
 
-    # copy, print, and amend the default config
-    config = ddpg.apex.APEX_DDPG_DEFAULT_CONFIG.copy()
-    #config["num_gpus"] = 0
-    config["num_workers"] = args.num_workers
-    config["horizon"] = 1000
-    #config["train_batch_size"] = 500
-    config["num_envs_per_worker"] = 4
-    config["gpu"] = args.gpu
-    #config["num_gpus_per_worker"] = 1/4
-    print(config)
-
-    # Assign model variables to commandline arguments
+    # assign model variables to commandline arguments
     ray.init(redis_address=args.redis_address)
 
-    # initialize an agent
-    agent = ddpg.apex.ApexDDPGAgent(config=config, env="my_env")
-
-    for i in range(10001):
-        # Perform one iteration of training
-        result = agent.train()
-        print(pretty_print(result))
-
-        if i % 100 == 0:
-            checkpoint = agent.save()
-            print("checkpoint saved at", checkpoint)
+    # run an experiment with a config
+    tune.run_experiments({
+        "my_experiment": {
+            "run": args.run,
+            "env": "KukaMultiBlocks-v0",
+            "stop": {"episode_reward_mean": 2000},
+            "checkpoint_freq": 20,
+            "checkpoint_at_end": 1,
+            "config": {
+                # "gpu": args.gpu,
+                "num_gpus": 1,
+                "num_workers": args.num_workers,
+                "horizon": 1000,
+                #"num_envs_per_worker": 4,
+            },
+        },
+    })
