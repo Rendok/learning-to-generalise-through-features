6c7806b12911479a3ed2f44c8adc8e9a3e502524
Miquel Ramírez <miquel.ramirez@gmail.com>
2019-01-23 12:07:21 +1100

    Update setup.py
    - Added ray to the list of dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_kuka_multi_blocks',
       version='0.0.2',
-      install_requires=['gym', 'pybullet', 'opencv-python', 'numpy']  # any extra dependencies
+      install_requires=['gym', 'pybullet', 'opencv-python', 'numpy', 'ray']  # any extra dependencies
 )
