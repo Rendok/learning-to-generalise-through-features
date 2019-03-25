24761ea0f7d607cb13e89543b9e847b059ed44e5
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2019-03-25 16:06:48 +1100

    Training test, reward tuning

--- gym_kuka_multi_blocks/envs/kuka_multi_blocks_gym_env.py
+++ gym_kuka_multi_blocks/envs/kuka_multi_blocks_gym_env.py
@@ -577,7 +577,7 @@ class KukaMultiBlocksEnv(KukaGymEnv):
                 return 50.0 + z * 10.0
             return -1.0
         else:
-            return - self.distance_x_y - self.distance_z - 10 * action_norm - action_fingers
+            return - 10 * self.distance_x_y - 10 * self.distance_z - action_norm - action_fingers
             #print("Delta d: {}, d: {}, ".format(self.pr_step_distance - d, d))
 
 
