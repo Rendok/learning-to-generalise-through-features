f479830b701488735e17715dd89555a2bd66038f
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2019-04-01 16:30:28 +1100

    New reward for place

--- gym_kuka_multi_blocks/envs/kuka_multi_blocks_gym_env.py
+++ gym_kuka_multi_blocks/envs/kuka_multi_blocks_gym_env.py
@@ -650,7 +650,7 @@ class KukaMultiBlocksEnv(KukaGymEnv):
                 p.stepSimulation()
             return 50
         else:
-            return - 10*self.distance_x_y - 10*self.distance_z - action_norm  # - action_fingers
+            return - 10*self.distance_x_y - 10*abs(self.distance_z - 0.125) - action_norm  # - action_fingers
 
     def _choose_block(self):
         """
