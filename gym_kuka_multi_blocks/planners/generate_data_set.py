f09c0a89b8f5af994e49277842c5a9f8874e0917
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2019-09-17 16:09:11 +1000

    Improved computational speed for a bigger dataset. (2)

--- gym_kuka_multi_blocks/planners/generate_data_set.py
+++ gym_kuka_multi_blocks/planners/generate_data_set.py
@@ -1,18 +1,18 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+import gym_kuka_multi_blocks.envs.kuka_cam_multi_blocks_gym as e
 
 
 def generate_random_data(length):
     assert type(length) is int
-    states = np.random.randn(length, 150, 150, 12)  # (W, H, C)
+    states = np.random.randn(length, 84, 84, 3)  # (W, H, C)
     actions = np.random.randn(length, 4, 1)
-    labels = np.random.randn(length, 150, 150, 12)  # (W, H, C)
+    labels = np.random.randn(length, 840, 84, 3)  # (W, H, C)
     return states, actions, labels
 
 
 def env_creator_kuka_cam(renders=False):
-    import gym_kuka_multi_blocks.envs.kuka_cam_multi_blocks_gym as e
     env = e.KukaCamMultiBlocksEnv(renders=renders,
                                   numObjects=3,
                                   isTest=4,
@@ -50,9 +50,9 @@ def generate_data(batch_size, planning_horizon):
 
     if DEBUG:
         print(batch_size*planning_horizon, 'inputs:', np.shape(inputs), 'labels:', np.shape(labels), 'actions:', np.shape(actions))
-        print('inputs:', inputs[-2:, 0, 0, 0], 'actions:', actions[-2:, 0], 'labels:', labels[-2:, 0, 0, 0])
+        print('inputs:', inputs[-2:, 0, 0, 0] / 255., 'actions:', actions[-2:, 0], 'labels:', labels[-2:, 0, 0, 0] / 255.)
 
-    return inputs, actions, labels
+    return inputs / 255., actions, labels / 255.
 
 
 def generate(n_batches, batch_size, planning_horizon, path):
@@ -69,24 +69,26 @@ def generate(n_batches, batch_size, planning_horizon, path):
     print('Data size:', data_size)
 
     with h5py.File(path, 'w') as f:
-        d_states = f.create_dataset('states', (data_size, 84, 84, 3), dtype='u1')
-        d_actions = f.create_dataset('actions', (data_size, 4))
-        d_labels = f.create_dataset('labels', (data_size, 84, 84, 3), dtype='u1')
+        d_states = f.create_dataset('states', (data_size, 84, 84, 3), compression="gzip")  # dtype='i4'
+        d_actions = f.create_dataset('actions', (data_size, 4), compression="gzip")
+        d_labels = f.create_dataset('labels', (data_size, 84, 84, 3), compression="gzip")
 
         for i in range(n_batches):
             bg = batch_size * planning_horizon * i
             end = batch_size * planning_horizon * i + batch_size * planning_horizon
             d_states[bg:end], d_actions[bg:end], d_labels[bg:end] = generate_data(batch_size, planning_horizon)
+            # if i % 2 == 0:
+            print('Batch {} of {}'.format(i+1, n_batches))
 
 
 if __name__ == "__main__":
 
-    path_tr = '/Users/dgrebenyuk/Research/dataset/training.h5'
-    path_val = '/Users/dgrebenyuk/Research/dataset/validation.h5'
+    path_tr = '/Users/dgrebenyuk/Research/dataset/training1.h5'
+    path_val = '/Users/dgrebenyuk/Research/dataset/validation1.h5'
     DEBUG = True
 
-    generate(n_batches=8, batch_size=64, planning_horizon=20, path=path_tr)
-    generate(n_batches=3, batch_size=64, planning_horizon=5, path=path_val)
+    generate(n_batches=5, batch_size=64, planning_horizon=20, path=path_tr)
+    generate(n_batches=2, batch_size=64, planning_horizon=20, path=path_val)
 
     # read data
     with h5py.File(path_tr, 'r') as f:
--- gym_kuka_multi_blocks/planners/gradient/model.py
+++ gym_kuka_multi_blocks/planners/gradient/model.py
@@ -2,8 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import tensorflow as tf
-import tensorflow_io.hdf5 as io
-# import ipykernel  # needed to fix a bug in console
+import tensorflow_io as tfio
 print(tf.__version__)
 
 
@@ -29,11 +28,8 @@ class AE(tf.keras.Model):
                 tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
                 tf.keras.layers.Dense(units=20 * 20 * 64, activation='relu'),
                 tf.keras.layers.Reshape(target_shape=(20, 20, 64)),
-                # tf.keras.layers.Conv2DTranspose(
-                #     filters=64, kernel_size=3, strides=(2, 2), padding="SAME", activation='relu'),
                 tf.keras.layers.Conv2DTranspose(
                     filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
-                # No activation
                 tf.keras.layers.Conv2DTranspose(
                     filters=3, kernel_size=4, strides=(2, 2), activation='sigmoid'),
             ]
@@ -78,11 +74,10 @@ def roll_out_plan(model, x0, actions):
     all_preds = tf.TensorArray(tf.float32, actions.shape[0])
 
     x_pred = model.encode(x0[np.newaxis, ...])
-    #for a in actions:
     for i in tf.range(actions.shape[0]):
         x_pred = model.forward(x_pred, actions[i, ...])
         all_preds.write(i, model.decode(x_pred))
-        # all_preds.append(model.decode(x_pred))
+
     return x_pred, all_preds.stack()
 
 
@@ -102,7 +97,6 @@ def plan(model, x0, xg, horizon, epochs):
     return actions
 
 
-@tf.function
 def compute_loss_de_en(model, x):
     z = model.encode(x)
     x_logit = model.decode(z)
@@ -116,9 +110,16 @@ def compute_loss_de_en(model, x):
     return loss
 
 
-@tf.function
-def compute_loss_env(model, x, a, y):
+def compute_apply_gradients_enc_dec(model, x, optimizer):
+    model.env_net.trainable = False
 
+    with tf.GradientTape() as tape:
+        loss = compute_loss_de_en(model, x)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+
+def compute_loss_env(model, x, a, y):
     x_pred, label = model.predict(x, a, y)
 
     loss = tf.reduce_sum(tf.losses.mean_squared_error(label, x_pred))
@@ -128,17 +129,6 @@ def compute_loss_env(model, x, a, y):
     return loss
 
 
-@tf.function
-def compute_apply_gradients_enc_dec(model, x, optimizer):
-    model.env_net.trainable = False
-    # model.inference_net.trainable = False
-    with tf.GradientTape() as tape:
-        loss = compute_loss_de_en(model, x)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-
-@tf.function
 def compute_apply_gradients_env(model, x, a, y, optimizer):
     model.inference_net.trainable = False
     model.generative_net.trainable = False
@@ -147,86 +137,102 @@ def compute_apply_gradients_env(model, x, a, y, optimizer):
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-def get_datasets(path_tr, path_val):
 
-    with h5py.File(path_tr, 'r') as f:
-        states = f['states'][:]
+# def get_datasets(path_tr, path_val):
+#     with h5py.File(path_tr, 'r') as f:
+#         states = f['states'][:1000]
+#
+#     # dataset = io.HDF5Dataset(path_tr, '/states')  # .shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+#     train_dataset = tf.data.Dataset.from_tensor_slices(states).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+#     del states
+#
+#     with h5py.File(path_val, 'r') as f:
+#         states = f['states'][:1000]
+#
+#     # test_dataset = io.HDF5Dataset(path_val, '/states')
+#     test_dataset = tf.data.Dataset.from_tensor_slices(states).batch(BATCH_SIZE)
+#     del states
+#     print(train_dataset.element_spec)
+#     return train_dataset, test_dataset
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(states).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
 
-    with h5py.File(path_val, 'r') as f:
-        states = f['states'][:]
-
-    test_dataset = tf.data.Dataset.from_tensor_slices(states).batch(BATCH_SIZE)
-    return train_dataset, test_dataset
-
-
-@tf.function
-def train_decoder(epochs, train_dataset, test_dataset):
-
-    # with h5py.File(path_tr, 'r') as f:
-    #     states = f['states'][:]  # / 255.0
-
-    # dataset = io.HDF5Dataset(path_tr, '/states') #.shuffle(TRAIN_BUF).batch(BATCH_SIZE)
-    # train_dataset = tf.data.Dataset.from_tensor_slices(states).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
-
-    # with h5py.File(path_val, 'r') as f:
-    #     states = f['states'][:]  # / 255.0
-
-    # test_dataset = io.HDF5Dataset(path_val, '/states')
-    # test_dataset = tf.data.Dataset.from_tensor_slices(states).batch(BATCH_SIZE)
-
-    for epoch in range(1, epochs + 1):
+def train_decoder(model, epochs, path_tr, path_val):
+    @tf.function
+    def train_decoder_one_step(model, train_dataset, test_dataset):
         for train_X in train_dataset:
-            # print(train_X.shape)
             compute_apply_gradients_enc_dec(model, train_X, optimizer)
 
-        train_loss = epoch_loss.result().numpy()
-        train_error = epoch_error.result().numpy()
+        train_loss = epoch_loss.result()
+        train_error = epoch_error.result()
         epoch_loss.reset_states()
         epoch_error.reset_states()
 
-        for i, test_X in enumerate(test_dataset):
+        for test_X in test_dataset.take(3):
             compute_loss_de_en(model, test_X)
 
-        print('Epoch', epoch, 'train loss:', train_loss, 'error:', train_error * 100,
-              'test loss:', epoch_loss.result().numpy(), 'error:', epoch_error.result().numpy() * 100, '\r')
+        return train_loss, train_error
+
+    # read datasets from hdf5
+    train_dataset = tfio.IOTensor.from_hdf5(path_tr)
+    train_dataset = train_dataset('/states').to_dataset().shuffle(TRAIN_BUF).batch(BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    test_dataset = tfio.IOTensor.from_hdf5(path_val)
+    test_dataset = test_dataset('/states').to_dataset().batch(BATCH_SIZE).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    for epoch in range(1, epochs + 1):
+        train_loss, train_error = train_decoder_one_step(model, train_dataset, test_dataset)
+        print('Epoch', epoch, 'train loss:', train_loss.numpy(), 'error:', train_error.numpy() * 100,
+          'test loss:', epoch_loss.result().numpy(), 'error:', epoch_error.result().numpy() * 100, '\r')
+
+        model.save_weights('/Users/dgrebenyuk/Research/dataset/weights/cp-de-{}.ckpt'.format(epoch))
 
         epoch_loss.reset_states()
         epoch_error.reset_states()
 
 
-def train_env(epochs, path_tr, path_val):
-
-    with h5py.File(path_tr, 'r') as f:
-        states = f['states'][:] / 255.0
-        actions = f['actions'][:]
-        labels = f['labels'][:] / 255.0
-
-        print(states.shape, actions.shape, labels.shape)
-        train_dataset = tf.data.Dataset.from_tensor_slices((states, actions, labels)).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
-
-    with h5py.File(path_val, 'r') as f:
-        states = f['states'][:] / 255.0
-        actions = f['actions'][:]
-        labels = f['labels'][:] / 255.0
-        test_dataset = tf.data.Dataset.from_tensor_slices((states, actions, labels)).batch(BATCH_SIZE)
-
-    for epoch in range(1, epochs + 1):
+def train_env(model, epochs, path_tr, path_val):
+    @tf.function
+    def train_env_one_step(model, train_dataset, test_dataset):
         for train_X, train_A, train_Y in train_dataset:
-            # print(np.shape(train_X), np.shape(train_A), np.shape(train_Y))
             compute_apply_gradients_env(model, train_X, train_A, train_Y, optimizer)
 
-        train_loss = epoch_loss.result().numpy()
-        train_error = epoch_error.result().numpy()
+        train_loss = epoch_loss.result()
+        train_error = epoch_error.result()
         epoch_loss.reset_states()
         epoch_error.reset_states()
 
         for train_X, train_A, train_Y in test_dataset:
             compute_loss_env(model, train_X, train_A, train_Y)
 
-        print('Epoch', epoch, 'train loss:', train_loss, 'error:', train_error * 100,
+        return train_loss, train_error
+
+    # read train datasets from hdf5
+    dataset = tfio.IOTensor.from_hdf5(path_tr)
+    ds_states = dataset('/states').to_dataset()
+    ds_actions = dataset('/actions').to_dataset()
+    ds_labels = dataset('/labels').to_dataset()
+
+    train_dataset = tf.data.Dataset.zip((ds_states, ds_actions, ds_labels))
+    train_dataset = train_dataset.shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+    del dataset, ds_states, ds_actions, ds_labels
+
+    # read validation datasets from hdf5
+    dataset = tfio.IOTensor.from_hdf5(path_val)
+    ds_states = dataset('/states').to_dataset()
+    ds_actions = dataset('/actions').to_dataset()
+    ds_labels = dataset('/labels').to_dataset()
+
+    test_dataset = tf.data.Dataset.zip((ds_states, ds_actions, ds_labels))
+    test_dataset = test_dataset.shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+    del dataset, ds_states, ds_actions, ds_labels
+
+    for epoch in range(1, epochs + 1):
+        train_loss, train_error = train_env_one_step(model, train_dataset, test_dataset)
+
+        print('Epoch', epoch, 'train loss:', train_loss.numpy(), 'error:', train_error.numpy() * 100,
               'test loss:', epoch_loss.result().numpy(), 'error:', epoch_error.result().numpy() * 100)
+
+        model.save_weights('/Users/dgrebenyuk/Research/dataset/weights/cp-de-{}.ckpt'.format(epoch))
 
         epoch_loss.reset_states()
         epoch_error.reset_states()
@@ -234,17 +240,17 @@ def train_env(epochs, path_tr, path_val):
 
 if __name__ == "__main__":
 
-    path_tr = '/Users/dgrebenyuk/Research/dataset/training.h5'
-    path_val = '/Users/dgrebenyuk/Research/dataset/validation.h5'
-    epochs = 2
+    path_tr = '/Users/dgrebenyuk/Research/dataset/training1.h5'
+    path_val = '/Users/dgrebenyuk/Research/dataset/validation1.h5'
+    epochs = 5
     TRAIN_BUF = 1000
     BATCH_SIZE = 100
     TEST_BUF = 1000
 
     optimizer = tf.keras.optimizers.Adam(1e-4)
     model = AE()
-    epoch_loss = tf.keras.metrics.Mean(name='train_loss')
-    epoch_error = tf.keras.metrics.MeanAbsoluteError(name='mean_abs_error')
+    epoch_loss = tf.keras.metrics.Mean(name='epoch_loss')
+    epoch_error = tf.keras.metrics.MeanAbsoluteError(name='epoch_abs_error')
 
     # print(model.inference_net.summary())
     # print('\n', model.generative_net.summary())
@@ -254,32 +260,22 @@ if __name__ == "__main__":
     model.load_weights(latest)
     print('Latest checkpoint:', latest)
 
-    tran_dataset, test_dataset = get_datasets(path_tr, path_val)
-    train_decoder(epochs, tran_dataset, test_dataset)
-    # train_env(epochs, path_tr, path_val)
+    train_decoder(model, epochs, path_tr, path_val)
+    # train_env(model, epochs, path_tr, path_val)
 
     # model.save_weights('/Users/dgrebenyuk/Research/dataset/weights/cp4_1.ckpt')
 
     number = 596
-    # mode = 'encode'
+    mode = 'encode'
     # mode = 'forward'
-    mode = 'rollout'
+    # mode = 'rollout'
     # mode = 'plan'
 
     with h5py.File(path_val, 'r') as f:
-        states = f['states'][number, :, :, :]  # / 255.0
+        states = f['states'][number, :, :, :]
         actions = f['actions'][number, :]
-        labels = f['labels'][number+4, :, :, :]  # / 255.0
+        labels = f['labels'][number+4, :, :, :]
         print('label act', actions)
-
-        # states = np.round(states, 0)
-        # print(states)
-
-        # for st in states:
-        #     plt.imshow(st)
-        #     plt.title('Env Transition States')
-        #     plt.axis('off')
-        #     plt.show()
 
         if mode == 'plan':
             # actions = np.array([[0, 0, -1, 0], [0, 0, -1, 0], [0, 0, -1, 0], [0, 0, -1, 0]]).astype(np.float32)
@@ -298,7 +294,7 @@ if __name__ == "__main__":
         if mode == 'rollout':
             actions = np.array([[0, 0, -1, 0], [0, 0, -1, 0], [0, 0, -1, 0], [0, 0, -1, 0]]).astype(np.float32)
             _, x_pred = roll_out_plan(model, states, actions)
-            print(x_pred)
+            # print(x_pred)
             for x_pr in x_pred:
                 plt.imshow(x_pr[0, :, :, :])
                 plt.title('Decoded Predicted Transition State')
