2f7ea683639832dc7544e37b409bb739c16d7e07
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2019-10-03 15:44:13 +1000

    VAE, decoder update

--- models/vae1_env_model.py
+++ models/vae1_env_model.py
@@ -1,49 +1,60 @@
+import tensorflow_probability as tfp
 import tensorflow as tf
+
+
+def make_prior(latent_dim):
+    return tfp.distributions.Independent(tfp.distributions.Normal(loc=tf.zeros(latent_dim), scale=1),
+                                         reinterpreted_batch_ndims=1)
 
 
 def make_inference_net(latent_dim):
     model = tf.keras.Sequential(
         [
             tf.keras.layers.InputLayer(input_shape=(128, 128, 6)),
+            tf.keras.layers.Lambda(lambda x: tf.cast(x, tf.float32) - 0.5),
             tf.keras.layers.Conv2D(
-                filters=32, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
+                filters=32, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.Conv2D(
-                filters=32, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
-            tf.keras.layers.Conv2D(
-                filters=64, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Conv2D(
-                filters=64, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
+                filters=32, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
             tf.keras.layers.Conv2D(
-                filters=128, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
+                filters=64, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.Conv2D(
-                filters=128, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
-            tf.keras.layers.Conv2D(
-                filters=256, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Conv2D(
-                filters=256, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
+                filters=64, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
             tf.keras.layers.Conv2D(
-                filters=512, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
+                filters=128, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.Conv2D(
-                filters=512, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
+                filters=128, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
+            tf.keras.layers.Conv2D(
+                filters=256, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.Conv2D(
+                filters=256, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
+            tf.keras.layers.Conv2D(
+                filters=512, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.Conv2D(
+                filters=512, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
             tf.keras.layers.Flatten(),
-            # tf.keras.layers.Dense(512, activation='relu',
-            #                       kernel_initializer=tf.keras.initializers.glorot_normal(seed=None)),
-            tf.keras.layers.Dense(2 * latent_dim)
+            tf.keras.layers.Dense(tfp.layers.MultivariateNormalTriL.params_size(latent_dim),
+                                  activation=None),
+            tfp.layers.MultivariateNormalTriL(latent_dim,
+                                              activity_regularizer=tfp.layers.KLDivergenceRegularizer(
+                                                  make_prior(latent_dim),
+                                                  weight=1.0)
+                                              ),
         ], name="encoder"
     )
     return model
@@ -53,46 +64,52 @@ def make_generative_net(latent_dim):
     model = tf.keras.Sequential(
         [
             tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
-            # tf.keras.layers.Dense(512, activation='relu',
-            #                       kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+
             tf.keras.layers.Dense(8192, activation='relu',
                                   kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            # tf.keras.layers.Reshape([1, 1, latent_dim]),
             tf.keras.layers.Reshape(target_shape=(4, 4, 512)),
             tf.keras.layers.UpSampling2D((2, 2)),
             tf.keras.layers.Conv2DTranspose(
-                filters=512, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
+                filters=512, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.Conv2DTranspose(
-                filters=512, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.UpSampling2D((2, 2)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=256, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=256, kernel_size=2, strides=(1, 1), activation='relu', padding='SAME',
+                filters=512, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.UpSampling2D((2, 2)),
             tf.keras.layers.Conv2DTranspose(
-                filters=128, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
+                filters=256, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.Conv2DTranspose(
-                filters=128, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.UpSampling2D((2, 2)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=64, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
-                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=64, kernel_size=3, strides=(1, 1), activation='relu', padding='SAME',
+                filters=256, kernel_size=2, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
                 kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
             tf.keras.layers.UpSampling2D((2, 2)),
             tf.keras.layers.Conv2DTranspose(
-                filters=32, kernel_size=3, strides=(1, 1), padding='SAME',
+                filters=128, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=128, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.UpSampling2D((2, 2)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            tf.keras.layers.UpSampling2D((2, 2)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=32, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+                kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            # tf.keras.layers.Conv2DTranspose(
+            #     filters=32, kernel_size=3, strides=(1, 1), activation=tf.nn.leaky_relu, padding='SAME',
+            #     kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
+            # tf.keras.layers.UpSampling2D((2, 2)),
+            tf.keras.layers.Conv2D(
+                filters=12, kernel_size=4, strides=(1, 1), activation=None, padding='SAME',
                 kernel_initializer=tf.keras.initializers.glorot_normal(seed=None)),
-            tf.keras.layers.Conv2DTranspose(
-                filters=6, kernel_size=4, strides=(1, 1), padding='SAME',
-                kernel_initializer=tf.keras.initializers.glorot_normal(seed=None)),
+            tf.keras.layers.Flatten(),
+            tfp.layers.IndependentLogistic((128, 128, 6), tfp.distributions.Logistic.sample),
         ], name="decoder"
     )
     return model
@@ -102,19 +119,19 @@ def make_latent_env_net(latent_dim):
     model = tf.keras.Sequential(
         [
             tf.keras.layers.InputLayer(input_shape=(latent_dim + 4)),
-            tf.keras.layers.Dense(2 * latent_dim, activation='relu',
+            tf.keras.layers.Dense(2 * latent_dim, activation=tf.nn.leaky_relu,
                                   kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Dense(2 * latent_dim, activation='relu',
+            tf.keras.layers.Dense(2 * latent_dim, activation=tf.nn.leaky_relu,
                                   kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Dense(2 * latent_dim, activation='relu',
+            tf.keras.layers.Dense(2 * latent_dim, activation=tf.nn.leaky_relu,
                                   kernel_initializer=tf.keras.initializers.he_normal(seed=None)),
-            tf.keras.layers.Dense(latent_dim, ),
+            tf.keras.layers.Dense(latent_dim, activation=None),
         ], name="dynamics"
     )
     return model
 
 
-class VAE(tf.keras.Model):
+class VAE1(tf.keras.Model):
     def __init__(self, latent_dim):
         super().__init__()
         self._latent_dim = latent_dim
@@ -130,35 +147,12 @@ class VAE(tf.keras.Model):
         return self.decode(eps, apply_sigmoid=True)
 
     def encode(self, x):
-        mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
-        return mean, logvar
+        z = self.inference_net(x)
+        return z
 
-    def reparameterize(self, mean, logvar):
-        eps = tf.random.normal(shape=mean.shape)
-        return eps * tf.exp(logvar * .5) + mean
-
-    def decode(self, z, apply_sigmoid=False):
-        logits = self.generative_net(z)
-        if apply_sigmoid:
-            probs = tf.sigmoid(logits)
-            return probs
-
-        return logits
-
-    @tf.function
-    def env_step(self, z, a):
-        # a = a[tf.newaxis, ...]
-        z = tf.concat([z, a], axis=1)
-        z_pred = self.lat_env_net(z)
-        return z_pred
-
-    @tf.function
-    def forward(self, x, a):
-        mean, logvar = self.encode(x)
-        z = self.reparameterize(mean, logvar)
-        z_pred = self.env_step(z, a)
-        y_pred = self.decode(z_pred, apply_sigmoid=True)
-        return y_pred
+    def decode(self, z):
+        rv_x = self.generative_net(z)
+        return rv_x
 
     def save_weights(self, nets, file_path, number):
         for ch in nets:
