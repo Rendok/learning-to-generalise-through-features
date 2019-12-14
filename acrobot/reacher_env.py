7f291c9647fafb60c02ed5b60b2b0be417afd3a1
Dmitry Grebenyuk <dgrebenyuk89@gmail.com>
2019-12-14 15:50:35 +1100

    Reacher train

--- acrobot/reacher_env.py
+++ acrobot/reacher_env.py
@@ -141,7 +141,7 @@ class ReacherBulletEnv(BaseBulletEnv, py_environment.PyEnvironment):
         # plt.imshow(self._encoding_net.decode(z)[0, ...])
         # plt.show()
 
-        return 4 - distance
+        return 3 - distance
 
     def get_observation(self):
         if self._obs_type == "float":
